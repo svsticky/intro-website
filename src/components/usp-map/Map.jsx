456a0fb5690e175ba3785736287ec3542d1f8094
Mstiekema <merstiekema@gmail.com>
2020-06-10 16:12:01 +0200

    Added bounds to the map

--- src/components/usp-map/Map.jsx
+++ src/components/usp-map/Map.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import { latLngBounds } from "leaflet";
 
 class MapPage extends Component {
   constructor() {
@@ -13,7 +14,8 @@ class MapPage extends Component {
   }
   
   render() {
-    const position = [this.coords.lat, this.coords.lng]
+    const position = [this.coords.lat, this.coords.lng];
+    const bounds = new latLngBounds([52.076361, 5.150056], [52.097342, 5.199981]);
 
     return (
       <main>
@@ -21,8 +23,10 @@ class MapPage extends Component {
           <h2>Ontdek de USP</h2>
         </article>
         <Map 
-          center={position} 
+          center={position}
           zoom={this.coords.zoom}
+          maxBounds={bounds}
+          minZoom={15}
         >
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
