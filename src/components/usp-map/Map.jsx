02df463c59e29850f3aa3227323e1de3a89ab8a7
Mstiekema <merijnstiekema@outlook.com>
2020-06-10 19:03:24 +0200

    Added base for information sidebar with dynamic content

--- src/components/usp-map/Map.jsx
+++ src/components/usp-map/Map.jsx
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 import { Map, TileLayer, Circle } from "react-leaflet";
 import { latLngBounds } from "leaflet";
 
+class Sidebar extends Component {
+  render() {
+    return (
+      <article className="sidebar" >
+        <h1>
+          {this.props.title}
+        </h1>
+        <p>
+          {this.props.content}
+        </p>
+      </article>
+    )
+  }
+}
+
 class MapPage extends Component {
   constructor() {
     super();
@@ -11,21 +26,34 @@ class MapPage extends Component {
       lng: 5.175657,
       zoom: 16
     }
+    this.state = {
+      title: "Test",
+      content: "Content"
+    }
+    this.title = "Test";
+    this.content = "Content"
   }
 
-  pointOfInterest(x) {
-    console.log(x);
+  pointOfInterest(title, content) {
+    console.log(title);
+
+    this.setState({
+      title: title,
+      content: content
+    })
   }
   
   render() {
     const position = [this.coords.lat, this.coords.lng];
     const bounds = new latLngBounds([52.076361, 5.150056], [52.097342, 5.199981]);
+    const educ = [52.085855, 5.171957];
 
     return (
       <main>
         <article>
           <h2>Ontdek de USP</h2>
         </article>
+        <Sidebar title={this.state.title} content={this.state.content} />
         <Map 
           center={position}
           zoom={this.coords.zoom}
@@ -37,9 +65,21 @@ class MapPage extends Component {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <Circle
+            center={educ}
+            radius={25}
+            onclick={() => this.pointOfInterest(
+              "Educatorium", 
+              "Welkom bij het Educatorium! Dit gebouw wordt vooral door INCA en INKU studenten gebruikt voor het afnemen van tentamens."
+            )}
+          >
+          </Circle>
+          <Circle
             center={position}
-            radius={20}
-            mouseover={this.pointOfInterest("Test")}
+            radius={25}
+            onclick={() => this.pointOfInterest(
+              "Test punt", 
+              "Hey hoi! Dit is een test punt. Met dit punt kan er namelijk getest worden of het klikken op points of interests werkt! :)"
+            )}
           >
           </Circle>
         </Map>
