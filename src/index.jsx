88b9b9d4816d0f6ce5e33ca275e195e5a1068d7f
Mstiekema <merstiekema@gmail.com>
2020-06-10 11:26:47 +0200

    Added router and base for home and map page

---
+++ src/index.jsx
@@ -0,0 +1,43 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from './serviceWorker';
+
+// Import components
+import Home from './components/Home.jsx';
+import Map from './components/usp-map/Map.jsx';
+
+// Import static content
+import './assets/index.css';
+import './assets/logo.svg';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <Router>
+        <main>
+          <Switch>
+            <Route path="/usp" component={Map} />
+            <Route component={Home} />
+          </Switch>
+        </main>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector("#root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
