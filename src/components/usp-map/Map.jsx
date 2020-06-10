c4e956bde035f84710b406d234c224b5820cc87e
Mstiekema <merijnstiekema@outlook.com>
2020-06-10 20:00:37 +0200

    Split up the locations and sidebar in seperate components

--- src/components/usp-map/Map.jsx
+++ src/components/usp-map/Map.jsx
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
-import { Map, TileLayer, Circle } from "react-leaflet";
+import { Map, TileLayer } from "react-leaflet";
 import { latLngBounds } from "leaflet";
 
-class Sidebar extends Component {
-  render() {
-    return (
-      <article className="sidebar" >
-        <h1>
-          {this.props.title}
-        </h1>
-        <p>
-          {this.props.content}
-        </p>
-      </article>
-    )
-  }
-}
+// Custom components
+import Sidebar from "./Sidebar";
+import Location from "./Location";
 
 class MapPage extends Component {
   constructor() {
@@ -26,17 +15,16 @@ class MapPage extends Component {
       lng: 5.175657,
       zoom: 16
     }
+
     this.state = {
       title: "Test",
       content: "Content"
     }
-    this.title = "Test";
-    this.content = "Content"
+
+    this.pointOfInterest = this.pointOfInterest.bind(this)
   }
 
   pointOfInterest(title, content) {
-    console.log(title);
-
     this.setState({
       title: title,
       content: content
@@ -46,7 +34,6 @@ class MapPage extends Component {
   render() {
     const position = [this.coords.lat, this.coords.lng];
     const bounds = new latLngBounds([52.076361, 5.150056], [52.097342, 5.199981]);
-    const educ = [52.085855, 5.171957];
 
     return (
       <main>
@@ -64,24 +51,18 @@ class MapPage extends Component {
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Circle
-            center={educ}
-            radius={25}
-            onclick={() => this.pointOfInterest(
-              "Educatorium", 
-              "Welkom bij het Educatorium! Dit gebouw wordt vooral door INCA en INKU studenten gebruikt voor het afnemen van tentamens."
-            )}
-          >
-          </Circle>
-          <Circle
-            center={position}
-            radius={25}
-            onclick={() => this.pointOfInterest(
-              "Test punt", 
-              "Hey hoi! Dit is een test punt. Met dit punt kan er namelijk getest worden of het klikken op points of interests werkt! :)"
-            )}
-          >
-          </Circle>
+          <Location
+            position={[52.085855, 5.171957]}
+            title="Educatorium"
+            content="Welkom bij het Educatorium! Dit gebouw wordt vooral door INCA en INKU studenten gebruikt voor het afnemen van tentamens."
+            handler={this.pointOfInterest}
+          />
+          <Location
+            position={position}
+            title="Test punt"
+            content="Hey hoi! Dit is een test punt. Met dit punt kan er namelijk getest worden of het klikken op points of interests werkt! :)"
+            handler={this.pointOfInterest}
+          />
         </Map>
       </main>
     );
