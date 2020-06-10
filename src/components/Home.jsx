49a08339d68f44c57ff76bef6a7c24f32e0c109d
Mstiekema <merstiekema@gmail.com>
2020-06-10 11:26:47 +0200

    Added router and base for home and map page

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <main>
+        <article>
+          <h2>INCA / INKU Introductie</h2>
+        </article>
+      </main>      
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/usp-map/Map.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Map extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <main>
+        <article>
+          <h2>Ontdek de USP</h2>
+        </article>
+      </main>      
+    );
+  }
+}
+
+export default Map;
