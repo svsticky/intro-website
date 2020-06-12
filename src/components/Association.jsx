ad4df9df01082ce15ce698bb33960a09b9b54d5a
Rick van Hoef <rickvhoef@gmail.com>
2020-06-12 16:28:27 +0200

    use grid for card styling

--- src/components/Association.jsx
+++ src/components/Association.jsx
@@ -4,12 +4,16 @@ const Association = ({ association }) => (
     <div className="ui row">
         <div className="ui fluid horizontal colored card">
             <div className="ui card content">
-                <h2 className="ui left aligned large header">{association.name}</h2>
-                <p>{association.text}</p>
+                <div className="ui grid">
+                    <div className="ten wide column">
+                        <h2 className="ui left aligned large header">{association.name}</h2>
+                        <p>{association.text}</p>
+                    </div>
+                    <div className="six wide column">
+                        <a href={association.link}><img className="ui right floated small image" src={require('../assets/logos/' + association.photo)}/></a>
+                    </div>
+                </div>
             </div>
-            <image className="content">
-                <a href={association.link}><img className="ui right floated small image" src={require('../assets/logos/' + association.photo)}/></a>
-            </image>
             <div className="extra meta">
                 <a href={association.link}><p>{association.link}</p></a>
             </div>
