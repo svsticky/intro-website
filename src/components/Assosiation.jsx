17a27dc897570ece4143e8bd8cb767b5efcb53f5
Kung Fu Donkey <sietze.riemersma@gmail.com>
2020-06-12 16:03:10 +0200

    association card done

--- src/components/Assosiation.jsx
+++ src/components/Assosiation.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Assosiation = ({ assosiation }) => (
     <div className="ui row">
-        <div className="ui fluid horizontal card">
+        <div className="ui fluid horizontal colored card">
             <div className="ui card content">
                 <h2 className="ui left aligned large header">{assosiation.name}</h2>
                 <p>{assosiation.text}</p>
             </div>
-            <div className="content">
-                <img className="ui right floated small image" src={require('../assets/' + assosiation.photo)}/>
+            <image className="content">
+                <a href={assosiation.link}><img className="ui right floated small image" src={require('../assets/' + assosiation.photo)}/></a>
+            </image>
+            <div className="extra meta">
+                <a href={assosiation.link}><p>{assosiation.link}</p></a>
             </div>
         </div>
     </div>
