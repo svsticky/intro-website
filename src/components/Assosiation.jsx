cc3f8a22e68a7e6681616b5d09b1437db96d9cca
Kung Fu Donkey <sietze.riemersma@gmail.com>
2020-06-12 15:46:40 +0200

    added logos to page

--- src/components/Assosiation.jsx
+++ src/components/Assosiation.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Assosiation = ({ assosiation }) => (
     <div className="ui row">
-        <div className="ui large header">
-            <p>{assosiation.name}</p>
-        </div>
-        <div>
-            <p>{assosiation.text}</p>
+        <div className="ui fluid horizontal card">
+            <div className="ui card content">
+                <h2 className="ui left aligned large header">{assosiation.name}</h2>
+                <p>{assosiation.text}</p>
+            </div>
+            <div className="content">
+                <img className="ui right floated small image" src={require('../assets/' + assosiation.photo)}/>
+            </div>
         </div>
     </div>
 );
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -2,20 +2,10 @@ import React from 'react';
 
 const Text = ({ text }) => (
     <div className="ui row">
-<<<<<<< HEAD
-        <div>
-            <p className="ui large header">{text.name}</p>
-        </div>
-        <div>
-            <p>{text.text}</p>
-        </div>
-
-=======
         <div className="ui collumn">
             <h2 className="ui left aligned large header">{text.name}</h2>
             <p>{text.text}</p>
         </div>
->>>>>>> 6d7249bb31e62179f14617ab28bd04a4ce18fda5
     </div>
 );
 export default Text;
