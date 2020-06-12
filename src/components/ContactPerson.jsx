00c6302034b437913c6db5b5fbde9d750705f06c
Kung Fu Donkey <sietze.riemersma@gmail.com>
2020-06-12 11:01:52 +0200

    added contactPersons

---
+++ src/components/ContactPerson.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContactPerson = ({ contactPerson }) => (
+<div className="card">
+    {contactPerson.name}
+</div>
+);
+
+export default ContactPerson;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
-
+import ContactPerson from './ContactPerson';
+import data from './data.json';
 class Home extends Component {
   render() {
     return (
       <main>
         <article>
           <h2>INCA / INKU Introductie</h2>
+          <div>
+            {getContactPersons(data.contactPersons)}
+          </div>
         </article>
       </main>      
     );
   }
+}
+const getContactPersons = contactPersons=>{
+  return contactPersons.map(contactPerson=>(
+  <ContactPerson contactPerson={contactPerson}/>
+  ))
 }
 
 export default Home;
