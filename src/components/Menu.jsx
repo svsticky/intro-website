a1a2cf2f44d93e64d3a1931af5956785e1f1e788
Rick van Hoef <rickvhoef@gmail.com>
2020-06-27 11:36:58 +0200

    hide menu on mobile

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import MenuItem from './MenuItem';
 import data from './data.json';
 
-const Menu = items => {
+const Menu = () => {
 	return(
-		<div className="ui inverted fixed pointing menu">
-      <div className="ui center aligned container">
-        <a className="item" href="#top">
-          <img
-            className="ui mini image"
-            src={require('../assets/logo.svg')}
-            alt="logo"
-          />
-        </a>
-        {
-          data.sections.map((item,index)=>(
-            <MenuItem item={item} key={index}/>
-          ))
-        }
+    <div className="ui grid">
+      <div className="ui computer only row">
+        <div className="ui inverted fixed pointing menu">
+          <div className="ui center aligned container">
+            <a className="item" href="#top">
+              <img
+                className="ui mini image"
+                src={require('../assets/logo.svg')}
+                alt="logo"
+              />
+            </a>
+            {
+              data.sections.map((item,index)=>(
+                <MenuItem item={item} key={index}/>
+              ))
+            }
+          </div>
+        </div>
       </div>
-		</div>
+    </div>
 	)
 }
 
