af05be903e68c61a2973eadbdb8cd998afae668a
Rick van Hoef <rickvhoef@gmail.com>
2020-06-27 11:23:28 +0200

    move menu to seperate file

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import MenuItem from './MenuItem';
+import data from './data.json';
 
-const Menu = ({ item }) => (
-    <a className="item" href={"#"+item.name}>
-        {item.name}
-    </a>
-);
+const Menu = items => {
+	return(
+		<div className="ui inverted fixed pointing menu">
+      <div className="ui center aligned container">
+        <a className="item" href="#top">
+          <img
+            className="ui mini image"
+            src={require('../assets/logo.svg')}
+            alt="logo"
+          />
+        </a>
+        {
+          data.sections.map((item,index)=>(
+            <MenuItem item={item} key={index}/>
+          ))
+        }
+      </div>
+		</div>
+	)
+}
 
 export default Menu;
