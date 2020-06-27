026ac00882b9a3e8d1759dff0769ba5bd71f5f78
Kung Fu Donkey <sietze.riemersma@gmail.com>
2020-06-27 13:39:25 +0200

    parallax scrolling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {findDOMNode} from 'react-dom';
 import ContactPerson from './ContactPerson';
 import Date from './Date';
 import data from './data.json';
@@ -7,53 +8,67 @@ import Text from './Text';
 import Association from "./Association";
 import Menu from "./Menu";
 import '../assets/home.css';
+import $ from "jquery";
 
 class Home extends Component {
   render() {
     return (
       <main>
         {getMenu(data.sections)}
+
         <a className="anchor" name = "top"/>
         <div className="ui main container">
-          <div className="video-container">
-            <iframe src="https://www.youtube.com/embed/ar9WRwCiSr0" title="themabekendmakingsvideo" frameborder="0"></iframe>
-          </div>
-          <div className="ui fluid card">
-            <div className="card content">
-              <a className="anchor" name="Title"/><p className="ui center aligned huge header">INCA / INKU Introductie</p>
-              {getText(data.introduction)}
+          <div className="parallax" ref="parallax">
+            <div className="video-container">
+              <iframe src="https://www.youtube.com/embed/ar9WRwCiSr0" title="themabekendmakingsvideo" frameborder="0"></iframe>
             </div>
-          </div>
-          <div className="ui fluid card">
-            <div className="card content">
-              <a className="anchor" name="Programma"/><p className="ui center aligned large header">Programma</p>
-              {getProgram(data.program)}
+            <div className="ui fluid card">
+              <div className="card content">
+                <a className="anchor" name="Title"/><p className="ui center aligned huge header">INCA / INKU Introductie</p>
+                {getText(data.introduction)}
+              </div>
             </div>
-          </div>
-          <div className="ui fluid card">
-            <div className="card content">
-              <a className="anchor" name="Verenigingen"/><p className="ui center aligned large header">Verenigingen</p>
-              {getAssociation(data.Associations)}
+            <div className="ui fluid card">
+              <div className="card content">
+                <a className="anchor" name="Programma"/><p className="ui center aligned large header">Programma</p>
+                {getProgram(data.program)}
+              </div>
             </div>
-          </div>
-          <div className="ui fluid card">
-            <div className="card content">
-              <a className="anchor" name="FAQ"/><p className="ui center aligned large header">FAQ</p>
-              {getFAQ(data.FAQ)}
+            <div className="ui fluid card">
+              <div className="card content">
+                <a className="anchor" name="Verenigingen"/><p className="ui center aligned large header">Verenigingen</p>
+                {getAssociation(data.Associations)}
+              </div>
             </div>
-          </div>
-          <div className="ui fluid card">
-            <div className="card content">
-              <a className="anchor" name="Contact"/><p className="ui center aligned large header">Contact</p>
-              <p className="ui centered grid">Bij voorkeur mailen vanwege wisselende telefonische beschikbaarheid in de zomer</p>
-              {getContactPersons(data.contactPersons)}
+            <div className="ui fluid card">
+              <div className="card content">
+                <a className="anchor" name="FAQ"/><p className="ui center aligned large header">FAQ</p>
+                {getFAQ(data.FAQ)}
+              </div>
+            </div>
+            <div className="ui fluid card">
+              <div className="card content">
+                <a className="anchor" name="Contact"/><p className="ui center aligned large header">Contact</p>
+                <p className="ui centered grid">Bij voorkeur mailen vanwege wisselende telefonische beschikbaarheid in de zomer</p>
+                {getContactPersons(data.contactPersons)}
+              </div>
             </div>
           </div>
         </div>
+        
       </main>
     );
   }
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.parallax);
+    console.log("event set");
+    this.par = findDOMNode(this.refs.parallax);
+  }
+  parallax = () =>{
+    $(this.par).css("top",$(document).scrollTop()/10);
+  }
 }
+
 const getMenu = items =>{
   return(
     <div className="ui inverted fixed pointing menu">
@@ -130,5 +145,6 @@ const getContactPersons = contactPersons=>{
     </div>
   )
 }
+
 
 export default Home;
