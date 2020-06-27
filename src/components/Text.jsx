f9ec4aa77342afa7af1bca68dc2bacfae2dd1086
Kung Fu Donkey <sietze.riemersma@gmail.com>
2020-06-27 14:09:54 +0200

    anchor placement

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 const Text = ({ text }) => (
     <div>
         <a className="anchor" name={text.name}/>
-        <div className="ui segment">
-            <h2 className="ui left aligned large header">{text.name}</h2>
-            <p>{text.text}</p>
-        </div>
+        <h2 className="ui left aligned large header">{text.name}</h2>
+        <p>{text.text}</p>
     </div>
 );
 export default Text;
