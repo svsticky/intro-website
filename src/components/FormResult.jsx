ccbb75d672fbb18557d0d9e92cd8e85349e93e41
TheCase99 <kees.szabo@gmail.com>
2020-07-23 21:54:04 +0200

    Changed the way messages are transmitted.
    Added the messages in the frontend instead of the backend. Deciding which one to use is based on the statuscode that we get from the backend.

--- src/components/FormResult.jsx
+++ src/components/FormResult.jsx
@@ -43,28 +43,47 @@ class FormResult extends Component {
 
   setMessage = () => {
     let parameters = (new URL(window.location)).searchParams;
-    let resultTitle = parameters.get('formResult');
-    let resultDescription = parameters.get('message');
+    let resultTitle = "Page was not loaded correctly";
+    let resultDescription = "Page was not loaded correctly";
     let resultCode = parseInt(parameters.get('code'));
 
-    document.getElementById("resultHeader").innerText = resultTitle;
-    document.getElementById("resultMessage").innerText = resultDescription;
 
     if(resultCode === 0){
+        resultTitle = "Je bent ingeschreven!";
+        resultDescription = "Gefeliciteerd, je bent ingeschreven voor de introductie! <br />" +
+            "Je gaat een geweldige tijd tegemoet. Geniet van je zomer en tot snel! ";
         let linkElement = document.getElementById("yellowLink");
         linkElement.href = "/";
         document.getElementById("yellowLinkText").innerText = "HOME"
     }
     else if(resultCode === 1){
+        resultTitle = "Er is iets mis gegaan :(";
+        resultDescription = "Er is bij het verwerken van je inschrijving iets mis gedaan. Probeer het alsjeblieft opnieuw. <br />" +
+            "Als het de volgende keer weer mis gaat, probeer het dan over een paar uur nog een keer.";
+        let linkElement = document.getElementById("yellowLink");
+        linkElement.href = "/";
+        document.getElementById("yellowLinkText").innerText = "OPNIEUW PROBEREN"
+    }
+    else if(resultCode === 3){
+        resultTitle = "Oeps, je hebt iets gedaan wat niet mag...";
+        resultDescription = "Ai, er zit een fout in je formulier. Volgens onze server klopt het volgende niet aan je formulier: <br />" +
+            " ".concat(parameters.get('message'));
         let linkElement = document.getElementById("yellowLink");
         linkElement.href = "/";
         document.getElementById("yellowLinkText").innerText = "OPNIEUW PROBEREN"
     }
     else {
+        resultTitle = "Volgens mij kennen we je al";
+        resultDescription = "Jouw studentennummer komt al in onze database voor, weet je zeker dat je je niet al een keer opgegeven hebt voor deze intro?<br />" +
+            "Mocht je zeker weten dat jij je nog niet ingeschreven hebt, neem dan concat op met de commissie, zij kunnen je inschrijven en uitzoeken wat er mis is gegaan.";
         let linkElement = document.getElementById("yellowLink");
         linkElement.href = "/#Contact";
         document.getElementById("yellowLinkText").innerText = "NAAR CONTACTGEGEVENS"
     }
+      document.getElementById("resultHeader").innerHTML = resultTitle;
+      document.getElementById("resultMessage").innerHTML = resultDescription;
+
+
 
 
   }
