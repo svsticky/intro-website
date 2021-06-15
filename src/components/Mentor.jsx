7606c59b2882c020809120249cc258c2a417c54e
Mstiekema <github@mstiekema.nl>
2021-06-15 09:38:23 +0200

    Updated info to 2021/2022 introduction

--- src/components/Mentor.jsx
+++ src/components/Mentor.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
 const Mentor = ({ mentor }) => (
-    <div className="ui segment">
+    <div className="ui segment mentor">
         <h2 className="ui left aligned large header">{mentor.name}</h2>
-        <img className="ui right floated medium spaced image mentor" src={require('../assets/logos/' + mentor.photo)} alt={mentor.name}/>
-	<h3>{mentor.study}</h3>
+        <img className="ui right floated medium spaced image" src={require('../assets/mentoren/' + mentor.photo)} alt={mentor.name}/>
+	    <h3>{mentor.real_name}</h3>
         <p>{mentor.text}</p>
-	<footer>{mentor.contactInfo}</footer>
     </div>
-
 );
+
 export default Mentor;
