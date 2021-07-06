d00c4d4b08fc369d0779530847a9a66ce2671fb1
jelte-akker <Jelte2410@gmail.com>
2021-07-06 21:54:25 +0200

    Tekst aanpassingen en invulling planning

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -7,13 +7,15 @@ const Date = ({ date }) => (
         {
             date.activities.map((activity, index) =>(
                 <div className = "ui row" key={index}>
+                    {/* tijdelijk nog niet nodig, wel voor vrijdag 9 juli
                     <div className = "ui three wide column">
                         <p>{activity.time}</p>
                     </div>
+                    */}
                     <div className = "ui four wide column">
                         <p>{activity.name}</p>
                     </div>
-                    <div className = "ui six wide column">
+                    <div className = "ui ten wide column">
                         <p>{activity.description}</p>
                     </div>
                 </div>
