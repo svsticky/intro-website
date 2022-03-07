9f8c694e8eae8b8cc25c5489b0f6b97a83ad2b71
Rick van Hoef <rickvhoef@gmail.com>
2022-03-07 22:11:36 +0100

    Fix requires and pages

--- src/components/Association.jsx
+++ src/components/Association.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Association = ({ association }) => (
     <div className="ui segment">
         <h2 className="ui left aligned large header">{association.name}</h2>
-        <a href={association.link}><img className="ui right floated medium spaced image association" src={require('../assets/logos/' + association.photo)} alt={association.name}/></a>
+        <a href={association.link}><img className="ui right floated medium spaced image association" src={'/' + association.photo} alt={association.name}/></a>
         <p>{association.text} <a href={association.textlink}>{association.textlink.substring(8)}</a></p>
     </div>
 
