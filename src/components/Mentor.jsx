718bdaea5d187f90a34b9376fdd23ecc21a2c6aa
Tobias de Bruijn <t.debruijn@array21.dev>
2023-08-20 21:09:40 +0200

    Add supermentor images

--- src/components/Mentor.jsx
+++ src/components/Mentor.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Mentor = ({ mentor }) => (
   <div className="ui segment mentor">
     <h2 className="ui left aligned large header">{mentor.name}</h2>
-    {/* <img className="ui right floated medium spaced image mentorImage" src={'/mentoren/' + mentor.photo} alt={mentor.name} /> */}
+    <img className="ui right floated medium spaced image mentorImage" src={'/mentoren/' + mentor.photo} alt={mentor.name} width="300"/>
     <h3>{mentor.real_name}</h3>
     <p className="formatted-text">{mentor.text}</p>
   </div>
