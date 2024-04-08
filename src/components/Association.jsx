4a7d00d4c8f4a7d289694e7a17dcf5869b6cd3b7
Tobias de Bruijn <t.debruijn@array21.dev>
2024-04-08 17:58:05 +0200

    Update logo; Mervin vond t niet leuk

--- src/components/Association.jsx
+++ src/components/Association.jsx
@@ -4,7 +4,7 @@ const Association = ({ association }) => (
     <div className="ui segment">
         <h2 className="ui left aligned large header">{association.name}</h2>
         <a href={association.link}>
-            <img className="ui right floated medium spaced image association" src={'/' + association.photo} alt={association.name}/>
+            <img className="ui right floated medium spaced image association" src='https://public.svsticky.nl/logos/logo_compact_outline_kleur.svg' alt={association.name}/>
         </a>
         <p><div dangerouslySetInnerHTML={{ __html: association.text}}></div> <a href={association.textlink}>{association.textlink.substring(8)}</a></p>
     </div>
