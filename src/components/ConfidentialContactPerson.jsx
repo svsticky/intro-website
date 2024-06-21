dfd83023388b77d22b07f8d9e61f4d8ebb6c2350
Tobias de Bruijn <t.debruijn@array21.dev>
2024-06-21 12:27:29 +0200

    Update olivier; add phone number

--- src/components/ConfidentialContactPerson.jsx
+++ src/components/ConfidentialContactPerson.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const ConfidentialContactPerson = ({ confidentialContactPerson }) => (
   <div className="ui segment confidentialContactPerson">
     <h2 className="ui left aligned large header">{confidentialContactPerson.name}</h2>
-    <img className="ui right floated medium spaced image confidentialContactPersonImage" src={'/confidentialContactPersons/' + confidentialContactPerson.image} alt={confidentialContactPerson.name} width="300"/>
+    <p className="ui left">
+      Telefoon: <a href={"tel:" + confidentialContactPerson.tel}
+                   className="ui left aligned">{confidentialContactPerson.tel}</a>
+    </p>
+    <img className="ui right floated medium spaced image confidentialContactPersonImage"
+         src={'/confidentialContactPersons/' + confidentialContactPerson.image} alt={confidentialContactPerson.name} width="300"/>
     <p className="formatted-text">{confidentialContactPerson.text}</p>
   </div>
 )
