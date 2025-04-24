fc703d0337bed4dc6d47d19dbd7c714505738863
Job Vonk <sticcpistt@icloud.com>
2025-04-24 23:28:27 +0200

    Added preloading of images in header, information and association

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preload from "astro-preload";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+      domains: ["images.eu.ctfassets.net", "images.ctfassets.net"],
+      remotePatterns: [{ protocol: "https" }]
+  },
+  integrations: [preload()]
+});
